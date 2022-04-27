77389d8b4a0d48abf882849d5bdfd63f500a89b9
Michael Ymbong <5406654+michaelymbong@users.noreply.github.com>
2022-04-27 18:32:41 +0800

    feat(app): move graphql schema definitions to root of project

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -1,36 +1,22 @@
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import { ApolloServer, ExpressContext, gql } from 'apollo-server-express';
+import { ApolloServer, ExpressContext } from 'apollo-server-express';
 import * as express from 'express';
 import { Server } from 'http';
 import Db from './db';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import { GRAPHQL_SCHEMA_PATH } from './constants';
+
+const SCHEMA = loadSchemaSync(GRAPHQL_SCHEMA_PATH, {
+  loaders: [new GraphQLFileLoader()],
+});
 
 export async function createApolloServer(
   db: Db,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
-  const typeDefs = gql`
-    type Query {
-      currentUser: User!
-      suggestions: [Suggestion!]!
-    }
-    type User {
-      id: String!
-      name: String!
-      handle: String!
-      coverUrl: String!
-      avatarUrl: String!
-      createdAt: String!
-      updatedAt: String!
-    }
-    type Suggestion {
-      name: String!
-      handle: String!
-      avatarUrl: String!
-      reason: String!
-    }
-  `;
-
   const resolvers = {
     Query: {
       currentUser: () => {
@@ -51,8 +37,7 @@ export async function createApolloServer(
   };
 
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: addResolversToSchema({ schema: SCHEMA, resolvers }),
     context: () => ({ db }),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
