169e3e0aac8ac608cfff7309242a832d359b1790
Michael Ymbong <5406654+michaelymbong@users.noreply.github.com>
2022-04-27 17:43:44 +0800

    feat(server): initial resolver for currentUser

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -5,7 +5,7 @@ import { Server } from 'http';
 import Db from './db';
 
 export async function createApolloServer(
-  _db: Db,
+  db: Db,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
@@ -30,10 +30,33 @@ export async function createApolloServer(
       reason: String!
     }
   `;
+
+  const resolvers = {
+    Query: {
+      currentUser: () => {
+        return {
+          id: '123',
+          name: 'John Doe',
+          handle: 'johndoe',
+          coverUrl: '',
+          avatarUrl: '',
+          createdAt: '',
+          updatedAt: '',
+        };
+      },
+      suggestions: () => {
+        return [];
+      },
+    },
+  };
+
   const server = new ApolloServer({
     typeDefs,
+    resolvers,
+    context: () => ({ db }),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
+
   await server.start();
   server.applyMiddleware({ app });
   return server;
